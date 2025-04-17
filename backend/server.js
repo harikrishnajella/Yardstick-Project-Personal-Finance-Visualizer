2158065d3352da2f1922edbd8b47540b77d774b9
jellaharikrishna <rk40665@gmail.com>
2025-04-17 13:17:55 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5001;
 const MONGO_URL = process.env.MONGO_URI || 'mongodb://localhost:27017'
 
 // Middleware
-app.use(cors(*));
+app.use(cors());
 app.use(express.json());
 
 // API Routes
